8f2260505c9da99ac84c2d625b0c3f5e9cc33b5e
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2025-06-24 11:58:17 +0200

    Umbau auf /dump Endpunkt (#4)

--- gen.php
+++ gen.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * This generated an overview of the current modules
+ * The result table has:
+ * -    the name of the module with the link to the git repository
+ * -    the status of the action Style
+ * -    the status of the action Tests
+ * -    a checkmark if the module is in the module store
+ * -    a mark if the module has a documentation link
+ *
+ * The base data are getting from the git api (https://api.github.com/user/14051084/repos?per_page=100)
+ * to compare to the store modules use the api from us (https://api.symcon.de/store/dump?language=de)
+ */
+
+/**
+ * Git Api = git api
+ * Store Api = our api
+ * Name: Git Api [name]
+ * Repo Link: Git Api [html_url]
+ * Style / Test Status: Workflow badge
+ * Store Checkmark: in_array(name, Store Api[modules][name])
+ * URL Mark: if module is in store and the link direct to page set a checkmark, else if the link directed to the git a red dot, else nothing
+ */
+
 $ignore = [
     "SymconBC",
     "SymconBroken",
@@ -10,23 +33,9 @@ $ignore = [
     "SymconWebinar",
     "WeidmannEmlog",
     "re",
-    "baresip"
+    "baresip",
+    "SymconStubs"
 ];
-
-$store = [
-    "Energiezaehler",
-    "Gardena",
-    "HomeConnect",
-    "Rechenmodule",
-    "SymconConfiguration",
-    "SymconGraph",
-    "SymconLJ",
-    "SymconMH",
-    "SymconREHAU",
-    "SymconReport",
-    "SymconSpotify"
-];
-
 $extern = [
     "SyncMySQL",
     "SymconREHAU",
@@ -34,27 +43,39 @@ $extern = [
     "SymconLJ",
     "FIWARE"
 ];
-
 $unfinished = [
     "Sonos",
     "WaermemengenZaehler",
 ];
-
+// Module has an documentation URL // Necessary for beta modules
 $url = [
-    "Alexa",
-    "Assistant",
-    "Gardena",
-    "HomeConnect",
-    "SymconLJ"
+    "site" => [
+        "StromAbrechnungsModul",
+        "VerbrauchsAlarm",
+        "VerbrauchInKategorie",
+        "VirtuelleGeraete",
+    ],
+    "git" => [
+        "Tronity",
+    ]
+];
+// Repo => ModuleStore
+$translation = [
+    "verbrauchzeitspanne" => "verbrauchinzeitspanne",
+    "ttsawspolly" => "texttospeechawspolly",
+    "symconbackup" => "backupftpftpssftp",
+    "symcongraph" => "webgraph",
+    "symconspotify" => "spotify",
+    "symconreport" => "reportmodul", 
 ];
 
+//get the github repos
 $opts = [
     "http" => [
         "method" => "GET",
         "header" => "User-Agent: Awesome-PHP\r\n"
     ]
 ];
-
 $context = stream_context_create($opts);
 
 $repos = json_decode(file_get_contents("https://api.github.com/user/14051084/repos?per_page=200", false, $context), true);
@@ -63,36 +84,57 @@ if (sizeof($repos) == 200) {
     die("We need to implement pagination ".sizeof($repos));
 }
 
-$modules = json_decode(file_get_contents("https://symcon-store.s3.eu-west-1.amazonaws.com/modules.json"));
+// get the modules from the dump endpoint
+$modules = json_decode(file_get_contents("https://api-dev.symcon.de/store/dump?language=de"), true)["modules"];
+$ourModules = array_filter($modules, function ($key): bool {
+    return strpos($key, "de.symcon") === 0;
+}, ARRAY_FILTER_USE_KEY);
+$modules = [];
+foreach ($ourModules as $bundleID => $module) {
+    foreach ($module as $channel) {
+        if($channel["channel"] === "stable") {
+            $moduleInstance = new Module($channel, $bundleID);
+            $modules[$moduleInstance->moduleName] = $moduleInstance;
+        }
+    }
+}
 
 function hasURL($name)
 {
     global $modules, $url;
-    foreach ($modules as $module) {
-        if (!isset($module->url)) {
-            continue;
-        }
-        if (strpos($module->url, "/symcon/" . $name) !== false) {
-            return true;
-        }
+    $module = getModule($name);
+    if($module !== false) {
+        return $module->urlStatus;
     }
     // Fallback to manually checked modules
-    return in_array($name, $url);
+    return in_array($name, $url["site"]) ? 2 : (in_array($name, $url["git"]) ? 1 : 0);
 }
 
-function isInStore($name)
+function isInStore($name): bool
 {
-    global $modules, $store;
-    foreach ($modules as $module) {
-        if ($module->name == $name) {
-            return true;
-        }
-    }
-    // Fallback to manually checked modules
-    return in_array($name, $store);
+    return getModule($name) !== false;
 }
 
-function filterRepo($repo)
+function getModule($name): Module|false
+{
+    global $modules, $translation;
+    
+    $moduleName = replaceSpecialCharacters(strtolower($name));
+    if(key_exists($moduleName, $modules)) {
+        return $modules[$moduleName];
+    }
+    if(key_exists($moduleName, $translation)) {
+        return $modules[$translation[$moduleName]];
+    }
+    foreach ($modules as $module) {
+        if($module->libraryName == $name) {
+            return $module;
+        }
+    }
+    return false;
+}
+
+function filterRepo($repo): bool
 {
     if (substr($repo, 0, 4) == "Skin") {
         return true;
@@ -109,6 +151,22 @@ function filterRepo($repo)
     return false;
 }
 
+function replaceSpecialCharacters(string $input): string|false
+{
+    // return preg_replace(["/[äÄ]/","/[üÜ]/","/[öÖ]/", "/ /","/-/", "/\(/", "/\)/", "/\//" ], ["ae", "ue", "oe", "","", "","", ""], $input)
+    $input = str_replace(" ", "", $input);
+    $input = str_replace("-", "", $input);
+    $input = str_replace(")", "", $input);
+    $input = str_replace("(", "", $input);
+    $input = str_replace("/", "", $input);
+    $input = strtolower($input);
+    $input = str_replace("ä", "ae", $input);
+    $input = str_replace("ü", "ue", $input);
+    $input = str_replace("ö", "oe", $input);
+    return $input;
+}
+
+//Result table
 $content = "### Übersicht aller PHP-Module der Symcon GmbH und deren Check Status" . PHP_EOL;
 $content .= PHP_EOL;
 $content .= "Name | Style | Tests | Store | URL" . PHP_EOL;
@@ -120,24 +178,32 @@ foreach ($repos as $repo) {
         continue;
     }
 
-    if (!in_array($repo["name"], $ignore)
-        && !in_array($repo["name"], $unfinished)
-        && !in_array($repo["name"], $extern)
+    if (in_array($repo["name"], $ignore)
+        || in_array($repo["name"], $unfinished)
+        || in_array($repo["name"], $extern)
     ) {
-        $content .= "[" . $repo["name"] . "](https://github.com/symcon/" . $repo["name"] . "/) | ";
-        $content .= "[![Check Style](https://github.com/symcon/" . $repo["name"] . "/workflows/Check%20Style/badge.svg)](https://github.com/symcon/" . $repo["name"] . "/actions)" . " | ";
-        $content .= "[![Run Tests](https://github.com/symcon/" . $repo["name"] . "/workflows/Run%20Tests/badge.svg)](https://github.com/symcon/" . $repo["name"] . "/actions)" . " | ";
-        if (isInStore($repo["name"])) {
-            $content .= " ✅";
-        }
-        $content .= " | ";
-        if (hasURL($repo["name"])) {
-            $content .= " ✅";
-        }
-        $content .= PHP_EOL;
-
-        $count++;
+        continue;
     }
+
+    $content .= "[" . $repo["name"] . "](" . $repo["html_url"] . "/) | ";
+    $content .= "[![Check Style](" . $repo["html_url"] . "/workflows/Check%20Style/badge.svg)](" . $repo["html_url"] . "/actions)" . " | ";
+    $content .= "[![Run Tests](" . $repo["html_url"] . "/workflows/Run%20Tests/badge.svg)](" . $repo["html_url"] . "/actions)" . " | ";
+
+
+    if (isInStore($repo["name"])) {
+        $content .= " ✅";
+    }
+    $content .= " | ";
+
+    $content .= match (hasURL($repo["name"])) {
+        0 => "",
+        1 => "🟠",
+        2 => "✅",
+        default => "",
+    };
+
+    $content .= PHP_EOL;
+    $count++;
 }
 
 $content .= "Aktuelle Anzahl der PHP-Module: " . $count . PHP_EOL;
@@ -154,7 +220,7 @@ foreach ($repos as $repo) {
     }
 
     if (in_array($repo["name"], $ignore)) {
-        $content .= "[" . $repo["name"] . "](https://github.com/symcon/" . $repo["name"] . "/) |" . PHP_EOL;
+        $content .= "[" . $repo["name"] . "](" . $repo["html_url"] . "/) |" . PHP_EOL;
     }
 }
 $content .= PHP_EOL;
@@ -171,7 +237,7 @@ foreach ($repos as $repo) {
     }
 
     if (in_array($repo["name"], $extern)) {
-        $content .= "[" . $repo["name"] . "](https://github.com/symcon/" . $repo["name"] . "/) |" . PHP_EOL;
+        $content .= "[" . $repo["name"] . "](" . $repo["html_url"] . "/) |" . PHP_EOL;
     }
 }
 $content .= PHP_EOL;
@@ -188,26 +254,41 @@ foreach ($repos as $repo) {
     }
 
     if (in_array($repo["name"], $unfinished)) {
-        $content .= "[" . $repo["name"] . "](https://github.com/symcon/" . $repo["name"] . "/) |" . PHP_EOL;
+        $content .= "[" . $repo["name"] . "](" . $repo["html_url"] . "/) |" . PHP_EOL;
     }
 }
 
 file_put_contents("README.md", $content);
 
-//Copy&Paste: https://www.php.net/manual/de/reserved.variables.httpresponseheader.php
-function parseHeaders($headers)
+class Module
 {
-    $head = array();
-    foreach ($headers as $k => $v) {
-        $t = explode(':', $v, 2);
-        if (isset($t[1])) {
-            $head[ trim($t[0]) ] = trim($t[1]);
-        } else {
-            $head[] = $v;
-            if (preg_match("#HTTP/[0-9\.]+\s+([0-9]+)#", $v, $out)) {
-                $head['reponse_code'] = intval($out[1]);
-            }
+    public String $moduleName;
+    public string $url;
+    public int $urlStatus; // 0 = missing, 1 = documentation is redirect to github, 2 = ok
+    public string $bundleID;
+    public string $libraryName;
+
+    public function __construct($module, $bundleID)
+    {
+        $replaceSpecialCharacters = function ($name): string {
+            $input = str_replace(" ", "", $name);
+            $input = str_replace("-", "", $input);
+            $input = str_replace(")", "", $input);
+            $input = str_replace("(", "", $input);
+            $input = str_replace("/", "", $input);
+            $input = strtolower($input);
+            $input = str_replace(["ä", "Ä"], "ae", $input);
+            $input = str_replace(["ü", "Ü"], "ue", $input);
+            $input = str_replace(["ö", "Ö"], "oe", $input);
+            return $input;
+        };
+
+        $this->moduleName = $replaceSpecialCharacters(strtolower($module["name"]));
+        $this->urlStatus = array_key_exists("documentation", $module) ? (strpos($module["documentation"], "/github") !== false ? 1 : 2) : 0;
+        if($this->urlStatus == 2) {
+            $this->url = $module["documentation"];
         }
+        $this->libraryName = json_decode($module["infoJSON"], true)["library"]["name"];
+        $this->bundleID = $bundleID;
     }
-    return $head;
 }
